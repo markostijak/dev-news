5d1610e58148dab89cf18bf119a551c634f86d82
Marko Stijak <marko.stijak@gmail.com>
2019-12-03 23:38:11 +0100

    Comments count fix

--- backend/src/main/java/com/stijaktech/devnews/domain/comment/CommentRepository.java
+++ backend/src/main/java/com/stijaktech/devnews/domain/comment/CommentRepository.java
@@ -1,21 +1,17 @@
 package com.stijaktech.devnews.domain.comment;
 
 import com.stijaktech.devnews.domain.post.Post;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 @RepositoryRestResource
-@SuppressWarnings("SpringCacheAnnotationsOnInterfaceInspection")
 public interface CommentRepository extends MongoRepository<Comment, String> {
 
     @Override
@@ -30,7 +26,6 @@ public interface CommentRepository extends MongoRepository<Comment, String> {
 
     List<Comment> findAllByPostId(@Param("id") String postId, Sort sort);
 
-    @Cacheable(cacheNames = "commentsCount", cacheManager = "ttlCache", key = "#post.id")
     int countByPost(@Param("post") Post post);
 
 }
